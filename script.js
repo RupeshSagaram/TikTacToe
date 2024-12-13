05c4c731f1c1c5f05a4725e00a54b6c7dbfefa74
Rupesh Sagaram <s.v.rupesh97@gmail.com>
2024-12-14 01:00:14 +0530

    removed minor bugs, playerSign visible after each round

--- script.js
+++ script.js
@@ -43,7 +43,8 @@ function gameController(
 
 
     const board = gameBoard();
-
+    const boardArray = board.getBoard();
+    console.log(boardArray);
     const players =[
         {
             name: playerOneName,
@@ -55,97 +56,84 @@ function gameController(
         }
     ];
 
-    let currentPlayer = players[0];
+    let currentPlayer = players[0].playerSign;
 
     const switchPlayerTurn = () =>{
-        currentPlayer = currentPlayer === players[0] ? players[1] :
-             players[0];
+        currentPlayer = currentPlayer === players[0].playerSign ? players[1].playerSign : players[0].playerSign;
+        // 'X' ? 'O' : 'X';
     };
 
-    const getActivePlayer = () => currentPlayer;
+    // const getActivePlayer = () => currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
 
     const printNewRound = () => {
         board.printBoard();
-        console.log(`${getActivePlayer.name}'s turn`);
+        console.log(`${currentPlayer}'s turn`);
     };
 
-    const playRound = () => {
-
-        function checkWin() {
-            // Check rows
-            for (let i = 0; i < 3; i++) {
-                if (board[i][0] === board[i][1] && board[i][1] === board[i][2] && board[i][0] !== ' ') {
-                    return board[i][0];
-                }
+    function checkWin() {
+        // Check rows
+        for (let i = 0; i < 3; i++) {
+            if (boardArray[i][0] === boardArray[i][1] && boardArray[i][1] === boardArray[i][2] && boardArray[i][0] !== ' ') {
+                return boardArray[i][0];
             }
-        
-            // Check columns
-            for (let i = 0; i < 3; i++) {
-                if (board[0][i] === board[1][i] && board[1][i] === board[2][i] && board[0][i] !== ' ') {
-                    return board[0][i];
-                }
-            }
-        
-            // Check diagonals
-            if (board[0][0] === board[1][1] && board[1][1] === board[2][2] && board[0][0] !== ' ') {
-                return board[0][0];
-            }
-            if (board[0][2] === board[1][1] && board[1][1] === board[2][0] && board[0][2] !== ' ') {
-                return board[0][2];
-            }
-        
-            return null;
         }
-
-        function isBoardFull() {
-            for (let i = 0; i < 3; i++) {
-                for (let j = 0; j < 3; j++) {
-                    if (board[i][j] === ' ') {
-                        return false;
-                    }
-                }
+    
+        // Check columns
+        for (let i = 0; i < 3; i++) {
+            if (boardArray[0][i] === boardArray[1][i] && boardArray[1][i] === boardArray[2][i] && boardArray[0][i] !== ' ') {
+                return boardArray[0][i];
             }
-            return true;
         }
-
-        while (true) {
-            board.printBoard();
-        
-            const row = prompt('Enter row (0-2): ');
-            const col = prompt('Enter column (0-2): ');
-        
-            if (row < 0 || row > 2 || col < 0 || col > 2 || board[row][col] !== ' ') {
-                console.log('Invalid move. Try again.');
-                continue;
-            }
-        
-            board[row][col] = currentPlayer;
-        
-            const winner = checkWin();
-            if (winner) {
-                console.log(`${winner} wins!`);
-                break;
-            } else if (isBoardFull()) {
-                console.log('Draw!');
-                break;
-            }
-        
-            switchPlayerTurn();
-            printNewRound();
-        
-        return{
-            playRound,
-            getActivePlayer
-        };    
+    
+        // Check diagonals
+        if (boardArray[0][0] === boardArray[1][1] && boardArray[1][1] === boardArray[2][2] && boardArray[0][0] !== ' ') {
+            return boardArray[0][0];
+        }
+        if (boardArray[0][2] === boardArray[1][1] && boardArray[1][1] === boardArray[2][0] && boardArray[0][2] !== ' ') {
+            return boardArray[0][2];
+        }
+    
+        return null;
     }
-
-
+    
+    function isBoardFull() {
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (boardArray[i][j] === ' ') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    while (true) {
+        printNewRound();
+    
+        const row = prompt('Enter row (0-2): ');
+        const col = prompt('Enter column (0-2): ');
+    
+        if (row < 0 || row > 2 || col < 0 || col > 2 || boardArray[row][col] !== ' ') {
+            console.log('Invalid move. Try again.');
+            continue;
+        }
+    
+        boardArray[row][col] = currentPlayer;
+    
+        const winner = checkWin();
+        if (winner) {
+            board.printBoard();
+            console.log(`${winner} wins!`);
+            break;
+        } else if (isBoardFull()) {
+            console.log('Draw!');
+            break;
+        }
+        
+       
+        switchPlayerTurn(); 
+    }
+    
 }
 
-}
-
-const game = gameController();
-
-
-
-gameController();
+// gameController();
